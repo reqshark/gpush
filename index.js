37a3cc94a708dcef05fb7e4a34e283c90a0541d3
Bent Cardan <bent@nothingsatisfies.com>
2014-07-03 19:29:58 -0400

    add server

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 
 
+var dgram = require('dgram');
 
 
 module.exports = function(argvs,cb){
 
-
+  if(argvs[2] == 'start'){
+    var address = '0.0.0.0'
+    var server = dgram.createSocket('udp4');
+    server.on('error',function(err){
+      console.log(err);
+      server.close();
+    });
+    server.on('message',function(msg,rinfo){
+      console.log(msg)
+    });
+    server.on('listening',function(){
+      var address = server.address();
+      console.log('server listening on ',address.address,':',address.port)
+    })
+    server.bind(41234,address);
+  }
 
 }
