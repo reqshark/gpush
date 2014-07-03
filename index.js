882a9d04cc42d4ed5935a9036fb4383e97ea3e9c
Bent Cardan <bent@nothingsatisfies.com>
2014-07-03 19:52:51 -0400

    test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 
 var dgram = require('dgram');
+var glob = require('glob');
 
 
 module.exports = function(argvs,cb){
@@ -20,6 +21,17 @@ module.exports = function(argvs,cb){
       console.log('server listening on ',address.address,':',address.port)
     })
     server.bind(41234,address);
+  }
+
+  if(argvs[2] == 'test'){
+    var client = dgram.createSocket('udp4');
+    var msg = new Buffer('test');
+    console.log(msg)
+    for(var i=0;i<20;i++){
+      client.send(msg,0,msg.length,41234,'192.241.163.88',function(err,bytes){
+        console.log(bytes)
+      })
+    }
   }
 
 }
