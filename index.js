886333d5ace576b264912db9258baa04db525f07
Bent Cardan <bent@nothingsatisfies.com>
2014-07-03 20:05:49 -0400

    add glob dep & do some err handle

--- index.js
+++ index.js
@@ -4,34 +4,36 @@ var dgram = require('dgram');
 var glob = require('glob');
 
 
-module.exports = function(argvs,cb){
+module.exports = function(argvs,fn){
 
-  if(argvs[2] == 'start'){
-    var address = '0.0.0.0'
-    var server = dgram.createSocket('udp4');
-    server.on('error',function(err){
-      console.log(err);
-      server.close();
-    });
-    server.on('message',function(msg,rinfo){
-      console.log(msg);
-    });
-    server.on('listening',function(){
-      var address = server.address();
-      console.log('server listening on ',address.address,':',address.port)
-    })
-    server.bind(41234,address);
-  }
-
-  if(argvs[2] == 'test'){
-    var client = dgram.createSocket('udp4');
-    var msg = new Buffer('test');
-    console.log(msg)
-    for(var i=0;i<20;i++){
-      client.send(msg,0,msg.length,41234,'192.241.163.88',function(err,bytes){
-        console.log(bytes)
+  if(argvs.length > 2){
+    if(argvs[2] == 'start'){
+      var address = '0.0.0.0'
+      var server = dgram.createSocket('udp4');
+      server.on('error',function(err){
+        console.log(err);
+        server.close();
+      });
+      server.on('message',function(msg,rinfo){
+        console.log(msg);
+      });
+      server.on('listening',function(){
+        var address = server.address();
+        console.log('server listening on ',address.address,':',address.port)
       })
+      server.bind(41234,address);
+    } else{
+      var client = dgram.createSocket('udp4');
+      var msg = new Buffer('test');
+      console.log(msg)
+      for(var i=0;i<20;i++){
+        client.send(msg,0,msg.length,41234,'192.241.163.88',function(err,bytes){
+          console.log(bytes)
+        })
+      }
     }
+  } else {
+    fn('not enough args passed.')
   }
 
 }
